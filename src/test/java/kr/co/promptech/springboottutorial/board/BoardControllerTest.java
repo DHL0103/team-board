45a610bfc8dd13f5a3f88c27e4a39d25b9438cef
daehee <daehee@promptech.co.kr>
2026-02-20 15:37:28 +0900

    test code for postService

--- src/test/java/kr/co/promptech/springboottutorial/board/BoardControllerTest.java
+++ src/test/java/kr/co/promptech/springboottutorial/board/BoardControllerTest.java
@@ -1,6 +1,6 @@
-package kr.co.promptech.springboottutorial;
+package kr.co.promptech.springboottutorial.board;
 
-import kr.co.promptech.springboottutorial.post.PostController;
+import kr.co.promptech.springboottutorial.post.Post;
 import kr.co.promptech.springboottutorial.post.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,28 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import java.util.Collections;
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-@WebMvcTest(PostController.class) // Controller만 가볍게 띄워서 테스트
-class PostControllerTest {
+
+@WebMvcTest(BoardController.class)
+class BoardControllerTest {
+
     @Autowired
-    private MockMvc mockMvc; // 가상의 HTTP 요청(GET, POST)을 보내는 역할을 하는 객체
+    private MockMvc mockMvc;
+
     @MockBean
-    private PostService postService; // 스프링 컨테이너 안에 가짜 Bean을 생성하여 등록
+    private PostService postService;
+
     @Test
-    @DisplayName("GET /board - 전체 게시글 조회 테스트")
+    @DisplayName("GET /board 요청 시 게시글 리스트와 함께 main_page 뷰를 반환해야 한다")
     void testGetAllBoardView() throws Exception {
         // given
+        // 서비스에서 데이터가 없다고 빈 리스트를 반환하게 설정 (PostControllerTest 내용 참고)
         given(postService.getAllPost()).willReturn(Collections.emptyList());
 
         // when & then
-        mockMvc.perform(get("/board"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("postList")) // HTML에서 postList로 쓰고 계셔서 수정했습니다.
-                .andExpect(view().name("main_page"));
+        mockMvc.perform(get("/board")) // /board 로 GET 요청을 보냄
+                .andExpect(status().isOk()) // HTTP 상태 코드가 200(OK)인지 확인
+                .andExpect(model().attributeExists("postList")); // Model에 "postList"라는 값이 담겼는지 확인
     }
 }
