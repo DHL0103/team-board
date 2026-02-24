f14f569c95f744e269f646193e19f8647401ea1a
daehee <daehee@promptech.co.kr>
2026-02-24 14:41:31 +0900

    feat:create PostFile

---
+++ src/main/java/kr/co/promptech/springboottutorial/model/PostFile.java
@@ -0,0 +1,20 @@
+package kr.co.promptech.springboottutorial.model;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class PostFile {
+
+    private Long id;
+
+    private Long postId;
+
+    private String originalName;
+
+    private String storedPath;
+
+    private Long fileSize;
+    
+    private LocalDateTime createdAt;
+}
