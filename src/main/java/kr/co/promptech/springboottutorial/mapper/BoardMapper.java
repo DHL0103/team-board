6d515ca4f296537e3e999adcd8bcdfc53abb49cf
daehee <daehee@promptech.co.kr>
2026-02-25 18:16:03 +0900

    feat : load boardList when load main Page

--- src/main/java/kr/co/promptech/springboottutorial/mapper/BoardMapper.java
+++ src/main/java/kr/co/promptech/springboottutorial/mapper/BoardMapper.java
@@ -1,4 +1,14 @@
 package kr.co.promptech.springboottutorial.mapper;
 
+import kr.co.promptech.springboottutorial.model.Board;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
 public interface BoardMapper {
+
+    @Select("SELECT * FROM boards")
+    List<Board> getAllBoards();
 }
--- src/main/java/kr/co/promptech/springboottutorial/service/BoardService.java
+++ src/main/java/kr/co/promptech/springboottutorial/service/BoardService.java
@@ -1,4 +1,19 @@
 package kr.co.promptech.springboottutorial.service;
 
+import kr.co.promptech.springboottutorial.mapper.BoardMapper;
+import kr.co.promptech.springboottutorial.model.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardMapper boardMapper;
+
+    public List<Board> getAllBoards() {
+        return boardMapper.getAllBoards();
+    }
 }
