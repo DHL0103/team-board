60e7c1fa82984a7312144c842af1af270d1c3783
daehee <daehee@promptech.co.kr>
2026-02-25 19:07:01 +0900

    feat : create 3 admin pages 2

--- src/main/java/kr/co/promptech/springboottutorial/controller/AdminController.java
+++ src/main/java/kr/co/promptech/springboottutorial/controller/AdminController.java
@@ -1,4 +1,32 @@
 package kr.co.promptech.springboottutorial.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
+
+    @GetMapping("/request")
+    public String requestPage(){
+
+
+        return "admin/request";
+    }
+
+    @GetMapping("/members")
+    public String memberPage(){
+
+
+        return "admin/members";
+    }
+    @GetMapping("/boards")
+    public String boardsPage(){
+
+
+        return "admin/boards";
+    }
 }
