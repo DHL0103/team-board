ba926b59b82e9f29e1d0cb8f80835c7d24aeb603
daehee <daehee@promptech.co.kr>
2026-02-26 15:33:25 +0900

    Fix: SecurityConfig에서 로그인 페이지 외에는 모두  막아둠

--- src/main/java/kr/co/promptech/springboottutorial/config/SecurityConfig.java
+++ src/main/java/kr/co/promptech/springboottutorial/config/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(
                         (authorizeHttpRequests) ->  authorizeHttpRequests
-                                .anyRequest().permitAll()
+                                .requestMatchers("/member/login").permitAll()
+                                .requestMatchers("/css/**", "/js/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .csrf((csrf) -> csrf.disable()) // 테스트 시에는 CSRF를 꺼두어야 Postman POST 요청이 잘 들어갑니다.
         ;
