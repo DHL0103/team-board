547475d3dd1f09248cb1f4ee711a7902ba13316f
daehee <daehee@promptech.co.kr>
2026-03-05 11:33:44 +0900

    Etc : BoardController getAllBoard 에 주석

--- src/main/java/kr/co/promptech/springboottutorial/controller/BoardController.java
+++ src/main/java/kr/co/promptech/springboottutorial/controller/BoardController.java
@@ -21,12 +21,16 @@ public class BoardController {
     private final BoardService boardService;
     private final MemberService memberService;
 
+    //로그인 이후 들어오는 메인페이지 로드
     @GetMapping
     public String getAllBoard(Model model, Principal principal) {
         Member currentMember = memberService.getMemberByUsername(principal.getName());
+        //유저의 시스템 레벨에 따라서 보여주는 보드 목록 구분
         if (currentMember.getRole().equals("ROLE_ADMIN")) {
+            //시스템 레벨 admin이라면 모든 보드 반환
             model.addAttribute("boardList", boardService.getAllBoards());
         } else {
+            //시스템 레벨 user라면 board_members 테이블을 이용하여 속해있는 보드만 조회
             model.addAttribute("boardList", boardService.getBoardsByMemberId(currentMember.getId()));
         }
         return "main_page";
