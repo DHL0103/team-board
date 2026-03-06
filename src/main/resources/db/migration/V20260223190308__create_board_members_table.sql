ebddc2b173ca7b02731c569117b0132b1bc3633e
daehee <daehee@promptech.co.kr>
2026-03-06 10:17:29 +0900

    etc : flyway board_members 테이블에 주석 추가

--- src/main/resources/db/migration/V20260223190308__create_board_members_table.sql
+++ src/main/resources/db/migration/V20260223190308__create_board_members_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE board_members (
     id         BIGINT      AUTO_INCREMENT PRIMARY KEY,
-    board_id   BIGINT      NOT NULL,
-    member_id  BIGINT      NOT NULL,
+    board_id   BIGINT      NOT NULL COMMENT '게시판 ID',
+    member_id  BIGINT      NOT NULL COMMENT '회원 ID',
     board_role VARCHAR(50) NOT NULL DEFAULT 'REQUESTED' COMMENT 'REQUESTED / USER / MANAGER',
     UNIQUE KEY uk_board_member (board_id, member_id),
     CONSTRAINT fk_bm_board  FOREIGN KEY (board_id)  REFERENCES boards(id)  ON DELETE CASCADE,
