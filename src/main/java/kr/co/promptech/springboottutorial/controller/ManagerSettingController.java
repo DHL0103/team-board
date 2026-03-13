5a71226a87ac861d1a9065f3defdc1568e9fbeac
daehee <daehee@promptech.co.kr>
2026-03-13 11:28:42 +0900

    feat : Manager Setting page 로드 구현

--- src/main/java/kr/co/promptech/springboottutorial/controller/ManagerSettingController.java
+++ src/main/java/kr/co/promptech/springboottutorial/controller/ManagerSettingController.java
@@ -1,4 +1,22 @@
 package kr.co.promptech.springboottutorial.controller;
 
+import kr.co.promptech.springboottutorial.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("board/{boardId}/manager/settings")
+@RequiredArgsConstructor
 public class ManagerSettingController {
+    private final BoardService boardService;
+
+    @GetMapping
+    public String settingsPage(Model model,@PathVariable Long boardId) {
+        model.addAttribute("board", boardService.getBoardDtoById(boardId));
+        return "manager/settings";
+    }   
 }
